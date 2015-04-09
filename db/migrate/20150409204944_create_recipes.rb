cedf7f0508a4da38621f471f52214bd52bd9e637
hayli <haylic@gmail.com>
2015-04-09 21:12:11 +0000

    Added model recipe with 3 fields: name, ingredients and preparations

---
+++ db/migrate/20150409204944_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+
+      t.string  :name
+      t.text    :ingredients, array: true, default: []
+      t.text    :preparations, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
